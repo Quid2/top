d7a4a486515fa932972c368d9c98d7a5c2361ed9
Pasqualino Titto Assini <tittoassini@gmail.com>
2019-09-28 10:43:42 +0200

    added new includes

--- src/Network/Top.hs
+++ src/Network/Top.hs
@@ -1,12 +1,14 @@
-module Network.Top (
--- |Check the <https://github.com/tittoassini/top tutorial and github repo>.
-module X) where
+-- | Check the <https://github.com/tittoassini/top tutorial and github repo>.
+module Network.Top
+  ( module X
+  ) where
 
-import           Data.Pattern           as X
-import           Network.Top.Pipes      as X
-import           Network.Top.Repo       as X
-import           Network.Top.Run        as X
-import           Network.Top.Types      as X
-import           Network.Top.Util       as X
-import           Network.Top.WebSockets as X
-import           ZM                     as X
+import           Data.Pattern         as X
+import           Network.Top.Function as X
+import           Network.Top.Pipes    as X
+import           Network.Top.Repo     as X
+import           Network.Top.Run      as X
+import           Network.Top.Types    as X
+import           Network.Top.Util     as X
+import           Network.Top.WSApp    as X
+import           ZM                   as X
